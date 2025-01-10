8fa3307ead8fc0b05f3e665777597cf580f59b8b
Braeden Turner <BraedenTurner@MacBook-Air.local>
2025-01-09 23:58:46 -0700

    Navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import styles from './App.modules.css';
 function App() {
 
   return (
-    <div className={styles.App}>Hello World</div>
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return <nav>
+        <a href='/'>Portfolio</a>
+    </nav>
+}
